9de64d574365a8b8689529e76574b126bfbb8beb
Marcel Stolin <marcelstolin@gmail.com>
2024-12-19 12:07:39 +0100

    Removed useless echoes

--- gen_tex.sh
+++ gen_tex.sh
@@ -4,8 +4,6 @@
 find data/ -type f -name "*.json" | while read file; do
     echo "Generate .tex for $file"
     filename=$(basename -- "${file}")
-    echo "${filename}"
     filename="${filename%.*}"
-    echo "${filename}"
     python gen_cv.py ${file} templates/jakes_resume.tex.jinja2 "${filename}.tex"
 done
