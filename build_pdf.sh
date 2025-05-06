ff8d6efcc4fca388360deb9434d383f2a2b1616b
Marcel Stolin <marcelstolin@gmail.com>
2025-05-06 16:57:34 +0200

    Updated build script to accept arguments

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Build all PDF files
-find . -type f -name "*.tex" | while read file; do
+find "${1:-.}" -type f -name "*.tex" | while read file; do
     echo "Build PDF for $file"
-    latexmk -interaction=nonstopmode -file-line-error -pdf -halt-on-error -shell-escape -outdir=_site ${file}
+    latexmk -interaction=nonstopmode -file-line-error -pdf -halt-on-error -shell-escape -outdir="${2:-.}" ${file}
 done
