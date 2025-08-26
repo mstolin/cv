51666778b095c7d91dab480ae2e8470767e68c20
Marcel <mstolin+github@posteo.me>
2025-08-26 10:35:33 +0200

    Fixed logs

--- build_tex.sh
+++ build_tex.sh
@@ -8,6 +8,7 @@ OUTPUT_DIR="${OUTPUT_DIR%/}"
 find "${INPUT_DIR}" -type f -name "*.json" | while read file; do
     filename=$(basename -- "${file}")
     filename="${filename%.*}"
-    echo "Build .tex { input: ${file}, output: ${OUTPUT_DIR}/${filename} }"
-    python3 gen_cv.py ${file} templates/jakes_resume.tex.jinja2 "${OUTPUT_DIR}/${filename}.tex"
+    output_file="${OUTPUT_DIR}/${filename}.tex"
+    echo "Build .tex { input: ${file}, output: ${output_file}} }"
+    python3 gen_cv.py ${file} templates/jakes_resume.tex.jinja2 "${output_file}"
 done
