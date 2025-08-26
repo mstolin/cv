b0857a02342409fcb5c9535332201b509a6183c6
Marcel <mstolin+github@posteo.me>
2025-08-26 09:46:21 +0200

    Updated build scripts to properly move files

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+INPUT_DIR="${1:-.}"
+OUTPUT_DIR="${2:-.}"
+
 # Build all PDF files
-find "${1:-.}" -type f -name "*.tex" | while read file; do
+find "${INPUT_DIR}" -type f -name "*.tex" | while read file; do
     echo "Build PDF for $file"
-    latexmk -interaction=nonstopmode -file-line-error -pdf -halt-on-error -shell-escape -outdir="${2:-.}" ${file}
+    latexmk -interaction=nonstopmode -file-line-error -pdf -halt-on-error -shell-escape -outdir="${OUTPUT_DIR}" ${file}
 done
--- gen_tex.sh
+++ gen_tex.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+INPUT_DIR="${1:-.}"
+OUTPUT_DIR="${2:-.}"
+
 # Generate all tex files
-find data/ -type f -name "*.json" | while read file; do
+find "${INPUT_DIR}" -type f -name "*.json" | while read file; do
     echo "Generate .tex for $file"
     filename=$(basename -- "${file}")
     filename="${filename%.*}"
-    python gen_cv.py ${file} templates/jakes_resume.tex.jinja2 "${filename}.tex"
+    python gen_cv.py ${file} templates/jakes_resume.tex.jinja2 "${OUTPUT_DIR}/${filename}"
 done
