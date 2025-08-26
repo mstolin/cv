7594f3c4cb109ad100ce16d11fb929276b69cfe6
Marcel <mstolin+github@posteo.me>
2025-08-26 10:24:24 +0200

    Fixed path bug with trailing slash

--- gen_tex.sh
+++ gen_tex.sh
@@ -2,11 +2,12 @@
 
 INPUT_DIR="${1:-.}"
 OUTPUT_DIR="${2:-.}"
+OUTPUT_DIR="${OUTPUT_DIR%/}"
 
 # Generate all tex files
 find "${INPUT_DIR}" -type f -name "*.json" | while read file; do
     filename=$(basename -- "${file}")
     filename="${filename%.*}"
     echo "Build .tex { input: ${file}, output: ${OUTPUT_DIR}/${filename} }"
-    python gen_cv.py ${file} templates/jakes_resume.tex.jinja2 "${OUTPUT_DIR}/${filename}"
+    python3 gen_cv.py ${file} templates/jakes_resume.tex.jinja2 "${OUTPUT_DIR}/${filename}.tex"
 done
