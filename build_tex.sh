d91f249873fcda57ee6730fb331263d0986162a6
Marcel <mstolin+github@posteo.me>
2025-09-04 10:43:17 +0200

    Updated to use uv

--- build_tex.sh
+++ build_tex.sh
@@ -9,6 +9,11 @@ find "${INPUT_DIR}" -type f -name "*.json" | while read file; do
     filename=$(basename -- "${file}")
     filename="${filename%.*}"
     output_file="${OUTPUT_DIR}/${filename}.tex"
+    args="gen_cv.py ${file} templates/jakes_resume.tex.jinja2 ${output_file}"
     echo "Build .tex { input: ${file}, output: ${output_file}} }"
-    python3 gen_cv.py ${file} templates/jakes_resume.tex.jinja2 "${output_file}"
+    if type "uv" > /dev/null; then
+        uv run ${args}
+    else
+        python3 ${args}
+    fi
 done
